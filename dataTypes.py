4670e1dde3807a1467943ac23665f172c5be4bae
Por Fok <porfok33@gmail.com>
2015-07-11 14:53:35 +0800

    hello world and data types

---
+++ dataTypes.py
@@ -0,0 +1,66 @@
+
+# Python Sample : Data Types
+# Version : Python 2.x
+'''
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+print 'Python Data Types'
+print 
+
+# Numbers
+a = 123
+print 'Print a number a = ' , a
+
+# Strings
+b = "The String"
+print 'Print a string b = ' , b
+
+# Lists
+c = [1, 2, 3, 4, 5]
+print 'Print a list c = ', c
+
+d = ['a', 'b', 'c']
+print 'Print a list d = ', d
+
+# Dictionaries
+e = {'temperature' : 25, 'weather' : 'sunny', 'mood' : 'good', 'money' : 0}
+print 'Print a Dictionaries e', e
+print 'Dictionary element e[''temperature''] = ', e['temperature']
+print 'Dictionary element e[''weather''] = ', e['weather']
+
+# Tuples
+f = (1, 2, 3, 4, 5)
+print 'Print a tuple f = ', f
+
+# Boolean
+g = True 
+h = False
+print 'Print a boolean g = ', g
+print 'Print a boolean h = ', h
+
+# Set
+i = set(['peter', 'mary', 'jenny'])
+j = set(['john', 'david', 'jenny'])
+l = i & j
+m = i | j
+print 'Print set i = ', i
+print 'Print set i = ', j
+print 'Print set i = ', l
+print 'Print set i = ', m
+
+
+
+
+
---
+++ helloWorld.py
@@ -0,0 +1,5 @@
+
+# Python Sample : Hello World
+# Version : Python 2.x
+
+print 'Hello World !!!'
